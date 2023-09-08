cf75a4e393222fb5177c449e960b0591b67f6757
fabianq846 <fabianq846@gmail.com>
2023-09-08 15:59:49 +0000

    Se soluciona el cierre de sesion, queda funcionando todo ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :job_postings
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'devise/sessions#new'
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
+  resources :job_postings
 end
